52af3b4473d1eab494cf5e1bceef9d4d771f4821
Alencar Avelar <alencarhlavelar@gmail.com>
2026-02-16 21:26:38 -0300

    chore: primeiro commit(initialz)

---
+++ CaseEquals/src/main/java/com/equals/CaseEquals/CaseEqualsApplication.java
@@ -0,0 +1,13 @@
+package com.equals.CaseEquals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseEqualsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseEqualsApplication.class, args);
+	}
+
+}
---
+++ CaseEquals/src/test/java/com/equals/CaseEquals/CaseEqualsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equals.CaseEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseEqualsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
